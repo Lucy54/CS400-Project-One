3f8877d4482307800e9d792769a93606bbf083d9
Lucy <lucykuo1@gmail.com>
2020-09-26 15:34:07 -0500

    third commit

--- src/main/java/frontend/Commands.java
+++ src/main/java/frontend/Commands.java
@@ -2,9 +2,11 @@ package main.java.frontend;
 
 public class Commands {
 
+  private BackEndMain backEnd = new BackEndMain();
+  
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    //
+    System.out.println(backEnd);
 
   }
 
